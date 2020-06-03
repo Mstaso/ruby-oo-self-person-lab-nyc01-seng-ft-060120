e32fcee912a999c3b17eb31b1e688bfbfa63b962
malcolm staso <mstaso1@gmail.com>
2020-06-03 22:12:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # your code goes here
+
+
+class Person
+  attr_reader :name
+  attr_accessor :bank_account
+  attr_accessor :happiness
+  
+  def initialize (name)
+    @name=name
+    @bank_account=25
+    @happiness=8
+  end  
+  
+  def happiness
+    self.happiness <= 10
+  end
+end
+

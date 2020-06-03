46ecc1e47552f8c58f3db249cf79506c93845482
malcolm staso <mstaso1@gmail.com>
2020-06-03 23:57:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -64,7 +64,7 @@ class Person
   def call_friend (friend)
     self.happiness = @happiness + 3
     friend.happiness = friend.happiness + 3
-    "Hi #{friend} it's #{self}."
+    "Hi #{friend} it's #{self}. How are you?"
     end
   end  
 end
